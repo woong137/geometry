0512ea2e7c7c740f924681e22aa76ea716e2666b
woong137 <woong137137@gmail.com>
2024-01-10 00:39:57 +0900

    Added scaling function to convert distance to nanometer

--- src/distance.cpp
+++ src/distance.cpp
@@ -12,4 +12,31 @@
 
 #include "geometry/distance.hpp"
 
-namespace programmers::geometry {}  // namespace programmers::geometry
+#include <cstdint>
+#include <sys/stat.h>
+
+namespace {
+auto ScaleDistanceToNanometer(double input_value,
+                              programmers::geometry::Distance::Type input_type)
+    -> int64_t {
+  int64_t result{static_cast<int64_t>(input_value)};
+  if (input_type == programmers::geometry::Distance::Type::kKilometer) {
+    result = static_cast<int64_t>(input_value * 1.0e+12);
+  } else if (input_type == programmers::geometry::Distance::Type::kMeter) {
+    result = static_cast<int64_t>(input_value * 1.0e+9);
+  } else if (input_type == programmers::geometry::Distance::Type::kCentimeter) {
+    result = static_cast<int64_t>(input_value * 1.0e+7);
+  } else if (input_type == programmers::geometry::Distance::Type::kMillimeter) {
+    result = static_cast<int64_t>(input_value * 1.0e+6);
+  } else if (input_type == programmers::geometry::Distance::Type::kMicrometer) {
+    result = static_cast<int64_t>(input_value * 1.0e+3);
+  }
+  return result;
+}
+}  // namespace
+
+namespace programmers::geometry {
+
+Distance::Distance(double input_value, Type input_type)
+    : nanometer_(ScaleDistanceToNanometer(input_value, input_type)) {}
+}  // namespace programmers::geometry
