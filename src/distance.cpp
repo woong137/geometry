102584a8f8e1b62cec1e70f06e27dafe2c4b4dc7
woong137 <woong137137@gmail.com>
2024-01-09 23:37:04 +0900

    Added Distance class declaration in geometry/distance.cpp

---
+++ src/distance.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file geometry/distance.cpp
+ * @author woong137 (woong137137@gmail.com)
+ * @brief Distance class declaration for abstracting distance
+ * @version 1.0.0
+ * @date 2024-01-09
+ * @copyright Copyright (c) 2024 Programmers, All Rights Reserved.
+ */
+
+// Copyright (c) 2024 Programmers, All Rights Reserved.
+// Authors: woong137
+
+#include "geometry/distance.hpp"
+
+namespace programmers::geometry {}  // namespace programmers::geometry
