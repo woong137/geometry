1b6716b821555c87e02153045f856d14a95802ca
woong137 <woong137137@gmail.com>
2024-01-09 19:21:30 +0900

    Added point2d.cpp file with Point class declaration

---
+++ src/point2d.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file geometry/point2d.hpp
+ * @author woong137 (woong137137@gmail.com)
+ * @brief Point class declaration with 2-dimension
+ * @version 1.0.1
+ * @date 2023-01-09
+ * @copyright Copyright (c) Programmers, All Rights Reserved.
+ */
+
+// Copyright (c) Programmers, All Rights Reserved.
+// Authors: woong137
+
