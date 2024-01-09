d5bfddbc2f9fa0561a96f5f3d732c6466fa38399
woong137 <woong137137@gmail.com>
2024-01-10 00:39:31 +0900

    Added Distance class constructor and assignment operators

--- include/geometry/distance.hpp
+++ include/geometry/distance.hpp
@@ -13,6 +13,7 @@
 #ifndef PROGRAMMERS__GEOMETRY__DISTANCE_HPP_
 #define PROGRAMMERS__GEOMETRY__DISTANCE_HPP_
 
+#include <cstdint>
 namespace programmers::geometry {
 /**
  * @brief The class for abstracting distance
@@ -30,10 +31,44 @@ class Distance {
     kMicrometer = 4,
     kNanometer = 5
   };
+  /**
+   * @brief Construct a new Distance object
+   */
+  Distance() = default;
+
+  explicit Distance(double input_value, Type input_type=Type::kMeter);
+
+  /**
+   * @brief The copy constructor
+   * @param other The other distance object
+   */
+  Distance(const Distance& other) = default;
+  /**
+   * @brief The move constructor
+   * @param other The other distance object
+   */
+  Distance(Distance&& other) = default;
+  /**
+   * @brief Destroy the Distance object
+   */
+  virtual ~Distance() = default;
+  /**
+   * @brief The copy assignment operator
+   * @param other The other distance object
+   * @return Distance& The reference of distance object
+   */
+  auto operator=(const Distance& other) -> Distance& = default;
+  /**
+   * @brief The move assignment operator
+   * @param other The other distance object
+   * @return Distance& The reference of distance object
+   */
+  auto operator=(Distance&& other) -> Distance& = default;
 
  protected:
  private:
-}; // class Distance
+  int64_t nanometer_{0};  ///< Nanometer
+};  // class Distance
 }  // namespace programmers::geometry
 
 #endif
