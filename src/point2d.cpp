d299999fbcf8d0844aa8a5832b8b707ff1b21d86
woong137 <woong137137@gmail.com>
2024-01-09 22:38:40 +0900

    Updated Point class with 2D developments

--- src/point2d.cpp
+++ src/point2d.cpp
@@ -1,7 +1,7 @@
 /**
  * @file geometry/point2d.cpp
  * @author woong137 (woong137137@gmail.com)
- * @brief Point class declaration with 2-dimension
+ * @brief Point class developments with 2-dimension
  * @version 1.0.1
  * @date 2023-01-09
  * @copyright Copyright (c) Programmers, All Rights Reserved.
@@ -10,3 +10,27 @@
 // Copyright (c) Programmers, All Rights Reserved.
 // Authors: woong137
 
+#include "geometry/point2d.hpp"
+
+#include <cmath>
+
+namespace programmers::geometry {
+Point2D::Point2D(double input_x, double input_y) : x_(input_x), y_(input_y) {}
+
+auto Point2D::CalculateDistance(const Point2D& target) const -> double {
+  return Point2D::CalculateDistance(*this, target);
+}
+
+auto Point2D::CalculateDistance(const Point2D& lhs,
+                                const Point2D& rhs) -> double {
+  return std::sqrt(std::pow((lhs.x_ - rhs.x_), 2) +
+                   std::pow((lhs.y_ - rhs.y_), 2));
+}
+
+auto Point2D::GetX() const -> double { return x_; }
+auto Point2D::GetY() const -> double { return y_; }
+
+auto Point2D::SetX(double input_x) -> void { x_ = input_x; }
+auto Point2D::SetY(double input_y) -> void { y_ = input_y; }
+
+}  // namespace programmers::geometry
