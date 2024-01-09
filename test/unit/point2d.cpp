7aea8969ddb8ba27eac5a4370f0e605f1bebc155
woong137 <woong137137@gmail.com>
2024-01-09 19:10:43 +0900

    Added unit test for Point2D constructor

---
+++ test/unit/point2d.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Programmers, All Rights Reserved.
+// Authors: woong137
+
+#include "geomtery/point2d.hpp"
+
+#include <limits>
+#include <stdexcept>
+
+#include "gtest/gtest.h"
+
+namespace programmers::geometry {
+  TEST(GeometryPoint2D, Constructor)
+}
