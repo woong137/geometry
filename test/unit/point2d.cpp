035489eefd4063dc4c8ba8beee0764c897ab6227
woong137 <woong137137@gmail.com>
2024-01-09 22:39:01 +0900

    Refactored geometry/point2d.hpp and add unit tests for Point2D class

--- test/unit/point2d.cpp
+++ test/unit/point2d.cpp
@@ -1,13 +1,104 @@
 // Copyright (c) Programmers, All Rights Reserved.
 // Authors: woong137
 
-#include "geomtery/point2d.hpp"
+#include "geometry/point2d.hpp"
 
-#include <limits>
+#include <cmath>
 #include <stdexcept>
 
 #include "gtest/gtest.h"
 
-namespace programmers::geometry {
-  TEST(GeometryPoint2D, Constructor)
+namespace {
+  constexpr uint32_t kTestCount = 1000U;
 }
+
+namespace programmers::geometry {
+TEST(GeometryPoint2D, Constructor) {
+  Point2D point1;
+  Point2D point2(0.0, 1.0);
+  Point2D point3(point2);
+  Point2D point4(std::move(Point2D()));
+}
+
+TEST(GeometryPoint2D, AssignmentOperator) {
+  Point2D point1;
+  auto Point2 = point1;
+  auto point3 = std::move(Point2D());
+}
+
+TEST(GeometryPoint2D, CalculateDistance) {
+  for (uint32_t i = 0; i < kTestCount; ++i) {
+    const auto kSourceX = static_cast<double>(std::rand());
+    const auto kSourceY = static_cast<double>(std::rand());
+    const auto kTargetX = static_cast<double>(std::rand());
+    const auto kTargetY = static_cast<double>(std::rand());
+
+    Point2D source(kSourceX, kSourceY);
+    Point2D target(kTargetX, kTargetY);
+
+    EXPECT_FLOAT_EQ(sqrt((kSourceX - kTargetX) * (kSourceX - kTargetX) +
+                         (kSourceY - kTargetY) * (kSourceY - kTargetY)),
+                    source.CalculateDistance(target));
+  }
+}
+
+TEST(GeometryPoint2D, StaticCalculateDistance) {
+  for (uint32_t i = 0; i < kTestCount; ++i) {
+    const auto kSourceX = static_cast<double>(std::rand());
+    const auto kSourceY = static_cast<double>(std::rand());
+    const auto kTargetX = static_cast<double>(std::rand());
+    const auto kTargetY = static_cast<double>(std::rand());
+
+    Point2D source(kSourceX, kSourceY);
+    Point2D target(kTargetX, kTargetY);
+
+    EXPECT_FLOAT_EQ(sqrt((kSourceX - kTargetX) * (kSourceX - kTargetX) +
+                         (kSourceY - kTargetY) * (kSourceY - kTargetY)),
+                    Point2D::CalculateDistance(source, target));
+  }
+}
+
+TEST(GeometryPoint2D, GetX) {
+  for (uint32_t i = 0; i < kTestCount; ++i) {
+    const auto kSourceX = static_cast<double>(std::rand());
+    const auto kSourceY = static_cast<double>(std::rand());
+
+    Point2D source(kSourceX, kSourceY);
+
+    EXPECT_FLOAT_EQ(kSourceX, source.GetX());
+  }
+}
+
+TEST(GeometryPoint2D, GetY) {
+  for (uint32_t i = 0; i < kTestCount; ++i) {
+    const auto kSourceX = static_cast<double>(std::rand());
+    const auto kSourceY = static_cast<double>(std::rand());
+
+    Point2D source(kSourceX, kSourceY);
+
+    EXPECT_FLOAT_EQ(kSourceY, source.GetY());
+  }
+}
+
+TEST(GeometryPoint2D, SetX) {
+  for (uint32_t i = 0; i < kTestCount; ++i) {
+    const auto kSourceX = static_cast<double>(std::rand());
+
+    Point2D source;
+    source.SetX(kSourceX);
+
+    EXPECT_FLOAT_EQ(kSourceX, source.GetX());
+  }
+}
+
+TEST(GeometryPoint2D, SetY) {
+  for (uint32_t i = 0; i < kTestCount; ++i) {
+    const auto kSourceY = static_cast<double>(std::rand());
+
+    Point2D source;
+    source.SetY(kSourceY);
+
+    EXPECT_FLOAT_EQ(kSourceY, source.GetY());
+  }
+}
+}  // namespace programmers::geometry
